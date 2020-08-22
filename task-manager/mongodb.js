ce1eb4e0015fa51fb82a72e19a950ab8c82fe529
musta7188 <musta7188@hotmail.it>
2020-08-22 19:25:36 +0100

    database connected

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -7,13 +7,17 @@ const mongodb = require("mongodb")
 // initialize the connection it give us access to the function we need to connect the database 
 const MongoClient = mongodb.MongoClient
 
-////used ip of the localhost 127.0.0.1
+
+//// choose the URL you want to connect to
+////used ip of the localhost 127.0.0.1 / followed by the port 27017
 const connectionURL = 'mongodb://127.0.0.1:27017'
 
+///name of the data base that can be anything you want 
 const databaseName = 'task-manager'
 
 ///connect method 
-///function that connect take the url and a callback function with error and client
+///function connect take the url and a callback function with error and client
+////option object "{ useNewUrlParser: true}" to be parse correctly to be passed to the server 
 MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => {
 
 if(error){
@@ -21,19 +25,41 @@ if(error){
 }
 
 ///db method to get the connection for a specific database
+////give back a db reference 
 const db = client.db(databaseName)
 
-///insert new data 
+///insert new data, with call back function 
 db.collection('users').insertOne({
   name: "Mustafa",
   age: 32
+    }, (error, result) => {
+      if(error) {
+        return console.log("unable to insert user")
+      }
+
+      console.log(result.ops)
     })
+
+
+
+
+
  
 })
+
+
+
+
+
+
+
+
+
+
 
 
 /// run the mongodb server on the console
 // cd ~ 
 // pwd 
 // Users/musta88
-/// Users/musta88/mongodb/bin/mongod --dbpath=/Users/musta88/mongodb-data
+/// /Users/musta88/mongodb/bin/mongod --dbpath=/Users/musta88/mongodb-data
