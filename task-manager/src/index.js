4124a293d0f3d1f469519e0635603ddfdca9bd2b
musta7188 <musta7188@hotmail.it>
2020-08-30 17:06:03 +0100

    endopoint taask

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,56 +1,87 @@
-const express =  require('express')
+    const express = require("express");
 
+    ///will ensure that the index is connected to the db, so we do need to require anything specific
+    require("./db/mongoos");
 
-///will ensure that the index is connected to the db, so we do need to require anything specific
-require('./db/mongoos')
+    const User = require("./models/user");
+    const Task = require("./models/task");
+const { ObjectID } = require("mongodb");
 
+    const app = express();
+    const port = process.env.PORT || 3000;
 
-const User = require('./models/user')
-const { collection } = require('./models/user')
+    ///transform the JSON data as an object to be able to access
+    app.use(express.json());
 
-const app = express()
-const port = process.env.PORT || 3000
+    app.post("/users", (req, resp) => {
+      const user = new User(req.body);
 
-///transform the JSON data as an object to be able to access 
-app.use(express.json())
+      user
+        .save()
+        .then(() => {
+          resp.status(201).send(user);
+        })
+        .catch((error) => {
+          resp.status(400).send(error);
+        });
+    });
 
-
-
-app.post('/users', (req, resp) =>{
-const user = new User(req.body)
-
-user.save().then(() => {
-
-  resp.send(user)
-
-}).catch((error) =>{
-  resp.status(400).send(error)
-})
-
-})
-
-
-app.delete('/users/:age', (req, resp) =>{
-
-
-  
-  User.remove({age: req.params.age === 0}, (error, resp) =>{
-    if (error){
-      return resp.send(error)
-    }
-
-    resp.send(resp)
-  })
-  
-  })
-  
-  
+    app.get('/users', (req, res) =>{
+      User.find({}).then((users) =>{
+        res.send(users)
+      }).catch((e) =>{
+        res.status(500).send()
+      })
+    })
 
 
 
+    app.get('/users/:id', (req, res) =>{
+      const _id = req.params.id
+
+      User.findById(_id).then((user) =>{
+        if(!user){
+          return res.status(404).send()
+        }
+
+        res.status(200).send(user)
+
+      }).catch((e) =>{
+        res.status(500).send(e)
+      })
+    })
 
 
-app.listen(port, () =>{
-  console.log('Server is up on port ' + port)
-})
 
+
+
+
+
+    app.post("/tasks", (req, resp) => {
+      const task = new Task(req.body);
+
+      task
+        .save()
+        .then(() => {
+          resp.status(201).send(task);
+        })
+        .catch((error) => {
+          resp.status(400).send(error);
+        });
+    });
+
+    // app.delete('/users/:age', (req, resp) =>{
+
+    //   User.remove({age: req.params.age === 0}, (error, resp) =>{
+    //     if (error){
+    //       return resp.send(error)
+    //     }
+
+    //     resp.send(resp)
+    //   })
+
+    //   })
+
+    app.listen(port, () => {
+      console.log("Server is up on port " + port);
+    });
---
+++ task-manager/src/models/task.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { ObjectID } = require("mongodb");
+const validator = require("validator")
+
+
+const Task = mongoose.model("Task", {
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  }
+
+})
+
+
+module.exports = Task
