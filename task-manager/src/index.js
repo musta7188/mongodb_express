da64b630f925007f51b5a61164bd6c7a1d70cf8c
musta7188 <musta7188@hotmail.it>
2020-09-01 14:21:18 +0100

    asyn method implement

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,87 +1,124 @@
-    const express = require("express");
+const express = require("express");
 
-    ///will ensure that the index is connected to the db, so we do need to require anything specific
-    require("./db/mongoos");
+///will ensure that the index is connected to the db, so we do need to require anything specific
+require("./db/mongoos");
 
-    const User = require("./models/user");
-    const Task = require("./models/task");
-const { ObjectID } = require("mongodb");
+const User = require("./models/user");
+const Task = require("./models/task");
 
-    const app = express();
-    const port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
-    ///transform the JSON data as an object to be able to access
-    app.use(express.json());
+///transform the JSON data as an object to be able to access
+app.use(express.json());
 
-    app.post("/users", (req, resp) => {
-      const user = new User(req.body);
+// app.post("/users", (req, resp) => {
+//   const user = new User(req.body);
 
-      user
-        .save()
-        .then(() => {
-          resp.status(201).send(user);
-        })
-        .catch((error) => {
-          resp.status(400).send(error);
-        });
-    });
+//   user
+//     .save()
+//     .then(() => {
+//       resp.status(201).send(user);
+//     })
+//     .catch((error) => {
+//       resp.status(400).send(error);
+//     });
+// });
 
-    app.get('/users', (req, res) =>{
-      User.find({}).then((users) =>{
-        res.send(users)
-      }).catch((e) =>{
-        res.status(500).send()
-      })
+/// recreate the above function in a sync way
+
+app.post("/users", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    //anything after this will wait until the user is saved
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+
+  //anything after this will wait until the user is saved
+});
+
+app.get("/users", async (req, res) => {
+
+  try {
+      const users = await User.find({})
+      res.status(202).send(users)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+  
+});
+
+app.get("/users/:id", async (req, res) => {
+  const _id = req.params.id
+
+  try {
+    const user = User.findById({_id})
+
+    if(!user){
+      return res.status(404).send("user not found try a different id")
+    }
+    res.send(user)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+  
+});
+
+app.get("/tasks", (req, resp) => {
+  Task.find({})
+    .then((tasks) => {
+      resp.status(200).send(tasks);
     })
+    .catch((e) => {
+      resp.status(500).send(e);
+    });
+});
 
+app.get("/tasks/:id", (req, resp) => {
+  const _id = req.params.id;
 
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return resp.status(404).send();
+      }
 
-    app.get('/users/:id', (req, res) =>{
-      const _id = req.params.id
-
-      User.findById(_id).then((user) =>{
-        if(!user){
-          return res.status(404).send()
-        }
-
-        res.status(200).send(user)
-
-      }).catch((e) =>{
-        res.status(500).send(e)
-      })
+      resp.status(200).send(task);
     })
-
-
-
-
-
-
-
-    app.post("/tasks", (req, resp) => {
-      const task = new Task(req.body);
-
-      task
-        .save()
-        .then(() => {
-          resp.status(201).send(task);
-        })
-        .catch((error) => {
-          resp.status(400).send(error);
-        });
+    .catch((e) => {
+      resp.status(500).send(e);
     });
+});
 
-    // app.delete('/users/:age', (req, resp) =>{
+app.post("/tasks", (req, resp) => {
+  const task = new Task(req.body);
 
-    //   User.remove({age: req.params.age === 0}, (error, resp) =>{
-    //     if (error){
-    //       return resp.send(error)
-    //     }
-
-    //     resp.send(resp)
-    //   })
-
-    //   })
-
-    app.listen(port, () => {
-      console.log("Server is up on port " + port);
+  task
+    .save()
+    .then(() => {
+      resp.status(201).send(task);
+    })
+    .catch((error) => {
+      resp.status(400).send(error);
     });
+});
+
+// app.delete('/users/:age', (req, resp) =>{
+
+//   User.remove({age: req.params.age === 0}, (error, resp) =>{
+//     if (error){
+//       return resp.send(error)
+//     }
+
+//     resp.send(resp)
+//   })
+
+//   })
+
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
+});
